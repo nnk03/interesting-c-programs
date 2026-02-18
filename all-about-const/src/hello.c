beb2f0e9e10f3685d626fc79b0723435643c44b8
Neeraj Krishna N <neerajnjps03@gmail.com>
2026-02-18 20:49:25 +0530

    added all-about-const

---
+++ all-about-const/src/hello.c
@@ -0,0 +1,8 @@
+#include "../include/hello.h"
+
+#include <stdio.h>
+
+void hello() {
+   fprintf( stdout, "Hello World!\n" );
+   return;
+}
---
+++ all-about-const/src/main.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+int main( int argc, char *argv[] ) {
+   // west const
+   /* const int x = 5; */
+   // east const - look right and that is what is actually made constant
+   int const x = 5;
+   // not valid
+   /* x = 10; */
+
+   // east const is usually consistent
+   // pointer to a const int
+   int const *a = &x;
+   a = &argc; // fine
+   // invalid
+   /* *a = 100; */
+
+   int y = 100;
+   // const pointer to an int
+   int *const b = &y;
+   *b = 1234; // fine
+   /* b = &x;    // invalid */
+
+   int const *const c = &x;
+   /* c = &y;    // invalid */
+   /* *c = 1234; // invalid */
+
+   return EXIT_SUCCESS;
+}
